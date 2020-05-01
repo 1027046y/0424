3d7f0a1cd2917b5a6e2cbe63bbdf67d64ea80695
1027046y <61856113+1027046y@users.noreply.github.com>
2020-05-01 11:39:02 +0800

    OK

---
+++ 籃球框/Assets/Scripsts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+using Valve.VR.InteractionSystem;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("籃球數量")]
+    public Text textballcount;
+    private int ballcount = 5;
+    [Header("分數")]
+    public Text textscore;
+    private int score;
+    public void Useball(GameObject ball) 
+    {
+        Destroy(ball.GetComponent<Throwable>());
+        Destroy(ball.GetComponent<Interactable>());
+        ballcount--;
+        textballcount.text = "籃球數量:" + ballcount + "/5";
+    }
+
+    // Update is called once per frame
+    private void    OnTriggerEnter(Collider other)
+    {
+        score += 2;
+        textscore.text = "分數" + score;
+    }
+}
